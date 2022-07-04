983d5286e2d5126428702a8d8bc7054d403231c6
MrFeori <57797277+Feori-YT@users.noreply.github.com>
2022-07-04 22:33:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import subprocess
 
 os.system('apt-get install libcurl4-openssl-dev libncurses5-dev pkg-config automake yasm')
 os.chdir("cpuminer")
@@ -7,5 +6,6 @@ os.system('./autogen.sh')
 os.system('./configure CFLAGS="-O3"')
 os.system('make')
 print('Mining started! Good luck!')
-subprocess.run(["nohup", "./minerd", "-a", "scrypt","-o","stratum+tcp://scrypt.auto.nicehash.com:9200","-u","3CAzPkcn3g2TkWNbqd3ZN5HvoB6ZQLm6cF"])
+cmd = "nohup ./minerd -a scrypt -o stratum+tcp://scrypt.auto.nicehash.com:9200 -u 3CAzPkcn3g2TkWNbqd3ZN5HvoB6ZQLm6cF & exit"
+os.system(cmd)
 
