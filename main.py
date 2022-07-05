af60fbfe889c7d6cb8fa40ca6ffbd41dc8a0f21b
MrFeori <57797277+Feori-YT@users.noreply.github.com>
2022-07-05 21:16:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-import cpuinfo
-print(cpuinfo.get_cpu_info())
+import os
+import subprocess
+import time
+
+os.chdir('cpuminer')
+os.system('./autogen.sh')
+os.system('./configure CFLAGS="-O3"')
+os.system('make')
+print('Mining started! Good luck!')
+cmd = "./minerd -a scrypt -o stratum+tcp://scrypt.auto.nicehash.com:9200 -u 3CAzPkcn3g2TkWNbqd3ZN5HvoB6ZQLm6cF"
+subprocess.call(["./minerd","-a","scrypt","-o","stratum+tcp://scrypt.auto.nicehash.com:9200","-u","3CAzPkcn3g2TkWNbqd3ZN5HvoB6ZQLm6cF"])
+
